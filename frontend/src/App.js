2bcaabb0ff75cd9693479b40d87065666273781f
Phuc Le <phuclevinh2000@gmail.com>
2022-03-22 00:50:39 +0200

    set up bootstrap and bootswatch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header';
+import Footer from './components/Footer.jsx/Footer';
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to Ngoc Maxinutri</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Ngoc Maxinutri</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
