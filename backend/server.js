ad33fc3b8728f187e02d0f660706cd3d8435c050
Phuc Le <phuclevinh2000@gmail.com>
2022-04-26 21:36:12 +0300

    update port name

--- backend/server.js
+++ backend/server.js
@@ -55,5 +55,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', function () {
-  console.log('Listening on Port 3000');
+  console.log('Listening on Port 5000');
 });
